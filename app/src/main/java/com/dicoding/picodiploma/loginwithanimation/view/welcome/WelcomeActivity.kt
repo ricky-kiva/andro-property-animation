20c668a0c8e7cd17abf8ded511aabfc35d92663a
ricky-kiva <ricky.kiva@gmail.com>
2023-04-17 15:48:06 +0700

    animation Welcome Actvt

--- app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/welcome/WelcomeActivity.kt
@@ -1,8 +1,11 @@
 package com.dicoding.picodiploma.loginwithanimation.view.welcome
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
@@ -20,6 +23,7 @@ class WelcomeActivity : AppCompatActivity() {
 
         setupView()
         setupAction()
+        playAnimation()
     }
 
     private fun setupView() {
@@ -42,6 +46,35 @@ class WelcomeActivity : AppCompatActivity() {
 
         binding.signupButton.setOnClickListener {
             startActivity(Intent(this, SignupActivity::class.java))
+        }
+    }
+
+    private fun playAnimation() {
+        // make `ObjectAnimator instance` of `Float`, `translateX` `imageView` from `-30f` to `30f`
+        ObjectAnimator.ofFloat(binding.imageView, View.TRANSLATION_X, -30f, 30f).apply {
+            // set animation duration
+            duration = 6000
+            // make repeat infinitely
+            repeatCount = ObjectAnimator.INFINITE
+            // animation will reverse back
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
+
+        // sets transparency animation from 0f to 1f, for 4 item
+        val login = ObjectAnimator.ofFloat(binding.loginButton, View.ALPHA, 1f).setDuration(500)
+        val signup = ObjectAnimator.ofFloat(binding.signupButton, View.ALPHA, 1f).setDuration(500)
+        val title = ObjectAnimator.ofFloat(binding.titleTextView, View.ALPHA, 1f).setDuration(500)
+        val desc = ObjectAnimator.ofFloat(binding.descTextView, View.ALPHA, 1f).setDuration(500)
+
+        // group animations to play together
+        val together = AnimatorSet().apply {
+            playTogether(login, signup)
+        }
+
+        // play animation sequentially
+        AnimatorSet().apply {
+            playSequentially(title, desc, together)
+            start()
         }
     }
 }
